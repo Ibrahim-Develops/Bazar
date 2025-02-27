6f575bc9e2228d049b7af8a2797498a2d92b2051
Ibrahim Develops <ibrahimdevelopss@gmail.com>
2025-02-27 19:12:30 +0500

    form

--- components/Form.tsx
+++ components/Form.tsx
@@ -5,6 +5,7 @@ import { Input } from "./ui/Input";
 import { Textarea } from "./ui/textarea";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react";
 
 type Inputs = {
     name: string,
@@ -15,12 +16,13 @@ type Inputs = {
 };
 
 const Form = () => {
+    const [data2, setData2] = useState<Inputs[]>([])
     const { register, handleSubmit, watch, formState: { errors, isSubmitting }, reset } = useForm<Inputs>();
     const names = watch("name")
     const onSubmit = (data: Inputs) => {
-    console.log(data);
-    toast.success("Form submitted successfully!", { autoClose: 1000 });
-    reset()
+        setData2([...data2, data])
+        toast.success("Form submitted successfully!", { autoClose: 1000 });
+        reset()
     }
 
     return (
@@ -28,7 +30,7 @@ const Form = () => {
             <div onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto p-4 border rounded-lg shadow-md mt-16">
                 <h2 className="text-xl font-semibold mb-4">Contact Form</h2>
                 <form className="space-y-4">
-                    <div>   
+                    <div>
                         <label className="block text-sm font-medium">Name</label>
                         <select {...register("name")} className="w-full p-2 border rounded">
                             <option>Select</option>
@@ -38,25 +40,25 @@ const Form = () => {
                     </div>
                     {names === "firstname" ? (
                         <>
-                        <div>
-                            <label className="block text-sm font-medium">First Name</label>
-                            <Input {...register("firstname", { required: "First Name Is Required" })} className={` w-full p-2 border-black border rounded focus:outline-black ${errors.firstname ? "border-red-500 focus:outline-red-400" : ""}`}/>
-                            {errors.firstname && <p className="text-red-500 text-sm mt-1">{errors.firstname.message}</p>}
-                        </div>
+                            <div>
+                                <label className="block text-sm font-medium">First Name</label>
+                                <Input {...register("firstname", { required: "First Name Is Required" })} className={` w-full p-2 border-black border rounded focus:outline-black ${errors.firstname ? "border-red-500 focus:outline-red-400" : ""}`} />
+                                {errors.firstname && <p className="text-red-500 text-sm mt-1">{errors.firstname.message}</p>}
+                            </div>
                         </>
                     ) : names === "secondname" ? (
                         <>
-                        <div>
-                            <label className="block text-sm font-medium">Second Name</label>
-                            <Input {...register("secondname", { required: "Second Name Is Required" })} className={`w-full p-2 border-black border rounded focus:outline-black ${errors.secondname ? "border-red-500 focus:outline-red-400" : ""}`}/>
-                            {errors.secondname && <p className="text-red-500 text-sm mt-1">{errors.secondname.message}</p>}
-                        </div>
+                            <div>
+                                <label className="block text-sm font-medium">Second Name</label>
+                                <Input {...register("secondname", { required: "Second Name Is Required" })} className={`w-full p-2 border-black border rounded focus:outline-black ${errors.secondname ? "border-red-500 focus:outline-red-400" : ""}`} />
+                                {errors.secondname && <p className="text-red-500 text-sm mt-1">{errors.secondname.message}</p>}
+                            </div>
                         </>
                     )
-                    : "" }
+                        : ""}
                     <div>
                         <label className="block text-sm font-medium">Email</label>
-                        <Input {...register("email", { required: "Email Is Required" })} className={`w-full p-2 border-black rounded focus:outline-black  ${errors.email ? "border-red-500 focus:outline-red-400" : ""}`}/>
+                        <Input {...register("email", { required: "Email Is Required" })} className={`w-full p-2 border-black rounded focus:outline-black  ${errors.email ? "border-red-500 focus:outline-red-400" : ""}`} />
                         {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>}
                     </div>
                     <div>
@@ -67,7 +69,18 @@ const Form = () => {
                     <button type="submit" disabled={isSubmitting} className="w-full bg-teal-600 text-white p-2 rounded hover:bg-teal-700">Submit</button>
                 </form>
             </div>
-            <ToastContainer/>
+            {data2.map((data2, i) => (
+                <div className="flex flex-col mx-10">
+                    <div key={i} className="p-4 border w-[300px] h-[300px] border-gray-300 rounded-lg shadow-md bg-white max-w-md mx-auto mt-4 space-y-2">
+                        <p className="text-lg font-semibold text-gray-700">{data2?.name}</p>
+                        <p className="text-gray-600">{data2?.firstname || data2?.secondname}</p>
+                        <p className="text-blue-600 font-medium">{data2?.email}</p>
+                        <p className="text-gray-500 italic">{data2?.message}</p>
+                    </div>
+                </div>
+            ))}
+
+            <ToastContainer />
         </div>
     )
 }
